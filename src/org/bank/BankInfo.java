1f66e43da7b78ea1a0ff71ff460bce362ee7ad0e
GeethaRD <geethanjaldt24@gmail.com>
2021-12-05 16:59:17 +0530

    bank

---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,23 @@
+package org.bank;
+
+public class BankInfo extends AxisBank {
+
+	private void rajesh() {
+		// TODO Auto-generated method stub
+
+		System.out.println("saving");
+
+	}
+
+	private void fixed() {
+		System.out.println("Fixed");
+	}
+
+	public static void main(String[] args) {
+
+		BankInfo b = new BankInfo();
+		b.fixed();
+		b.rajesh();
+	}
+
+}
